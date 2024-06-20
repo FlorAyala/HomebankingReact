93d008d3db9ca84c9bcd220bb391cb462532f343
Florencia Ayala <flor.ayala30@gmail.com>
2024-06-20 16:53:47 -0300

    se elimino un import en cards

--- src/components/CardsCreditDebit.jsx
+++ src/components/CardsCreditDebit.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-import 'react-credit-cards/es/styles-compiled.css';
+
 
 const CardsCreditDebit = (props) => {
  
